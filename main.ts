4fcb4e8ac69f83a92f4633bc84f4953091e8073d
2emmikas <160681305+2emmikas@users.noreply.github.com>
2024-02-21 07:52:36 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,28 @@
+/**
+ * steer right
+ */
+/**
+ * steer left
+ */
+/**
+ * accelerator button
+ */
+input.onButtonPressed(Button.A, function () {
+    keyboard.sendString("w")
+})
+/**
+ * brakes
+ */
+input.onButtonPressed(Button.B, function () {
+    keyboard.sendString("s")
+})
+basic.forever(function () {
+    if (input.rotation(Rotation.Roll) > 35) {
+        keyboard.sendString("d")
+    }
+})
+basic.forever(function () {
+    if (input.rotation(Rotation.Roll) > -35) {
+        keyboard.sendString("a")
+    }
+})
